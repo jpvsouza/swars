d834c603589c381e4e92425192c6889611da3421
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-12-07 07:42:33 -0300

    Talvez o primeiro requisito tenha ido

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,7 +13,18 @@ export default function Table() {
               value.map((planet) => (
                 <tr key={ planet.name }>
                   <td>{planet.name}</td>
-                  <td>{planet.orbit}</td>
+                  <td>{planet.rotation_period}</td>
+                  <td>{planet.orbital_period}</td>
+                  <td>{planet.diameter}</td>
+                  <td>{planet.climate}</td>
+                  <td>{planet.gravity}</td>
+                  <td>{planet.terrain}</td>
+                  <td>{planet.surface_water}</td>
+                  <td>{planet.population}</td>
+                  <td>{planet.films}</td>
+                  <td>{planet.created}</td>
+                  <td>{planet.edited}</td>
+                  <td>{planet.url}</td>
                 </tr>
               ))
             }
